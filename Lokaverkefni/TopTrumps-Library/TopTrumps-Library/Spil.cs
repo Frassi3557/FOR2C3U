70a6ffa1002f74ec9dabd18466add6854c7a439e
arontanatip <aront.99@hotmail.com>
2017-04-06 08:50:34 +0000

    Base class

---
+++ Lokaverkefni/TopTrumps-Library/TopTrumps-Library/Spil.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopTrumps_Library
+{
+    public class Spil
+    {
+
+        public string Nafn { get; set; }
+        public int Þyngd { get; set; }
+        public int Mjolkurlagni { get; set; }
+        public int EinkunnUllar { get; set; }
+        public int EinkunnLæris { get; set; }
+        public int Frjosemi { get; set; }
+        public int Gerd { get; set; }
+        public int EinkunFyrirMalir { get; set; }
+
+        public Spil(string nafn, int þyngd, int mjolkurlagni, int einkunnUllar, int einkunnLæris, int frjosemi, int gerd, int einkunFyrirMalir)
+        {
+            Nafn = nafn;
+            Þyngd =þyngd;
+            Mjolkurlagni = mjolkurlagni;
+            EinkunnUllar = einkunnUllar;
+            EinkunnLæris = einkunnLæris;
+            Frjosemi = frjosemi;
+            Gerd = gerd;
+            EinkunFyrirMalir = einkunFyrirMalir;
+        }
+
+       
+    }
+}
